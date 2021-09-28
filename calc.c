a69300e51af2f90126c906063ccaa3d8658a3218
g1b3rMvn15 <65787753+g1b3rMvn15@users.noreply.github.com>
2021-09-27 21:02:23 -0300

    Versao1

--- calc.c
+++ calc.c
@@ -1,5 +1,5 @@
 /* ATIVIDADE FIAP ON - CURSO DEFESA CIBERNETICA CAP-3 C, a linguagem primordial - O futuro se escreve em C
-Aluno: Glauber Lopes da Costa Muniz - 1TDCO2021 RM 87682 
+Aluno: Glauber Lopes da Costa Muniz - 1TDCO2021 
 PROJETO: CALCULADORA SIMPLES
 */
 
